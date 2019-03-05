fa4b95a6e2cce57dcb08e99f1c95fe74ba39cb0a
honnuanand <anand@honnu.com>
2019-03-05 00:55:05 -0800

    solution 02    to labs/my_work

---
+++ labs/solutions/02/cloud-native-spring/src/main/java/io/pivotal/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package io.pivotal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
