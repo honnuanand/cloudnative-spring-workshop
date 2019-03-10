1dcb35eace016ab0b78eec4c04681c6b3a3f1475
honnuanand <anand@honnu.com>
2019-03-09 18:01:02 -0800

    solution 04 initial

---
+++ labs/solutions/04/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+}
