2d35761d488535f5005ceb56e7875b96f79e54db
honnuanand <anand@honnu.com>
2019-03-17 19:42:51 -0700

    solution 05 initial

---
+++ labs/solutions/05/cloud-native-spring/src/main/java/io/pivotal/CloudNativeSpringApplication.java
@@ -0,0 +1,31 @@
+package io.pivotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class CloudNativeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNativeSpringApplication.class, args);
+	}
+
+	@Order(105)
+	@Profile("!cloud")
+	@Configuration
+	static class ApplicationSecurityOverride extends WebSecurityConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity web) throws Exception {
+			web.authorizeRequests().antMatchers("/**").permitAll();
+			web.csrf().disable();
+
+		}
+		
+	}
+}
