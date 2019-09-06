4014f04aa87136fd9be3111c0c8d7914fc1b0aa7
ssinghalpvtl <ssinghal@pivotal.io>
2019-09-06 12:43:36 -0700

    Updated package stucture

--- labs/solutions/05/cloud-native-spring-ui/src/test/java/com/pivotal/cloudnativespringui/CloudNativeSpringUiApplicationTests.java
+++ labs/solutions/05/cloud-native-spring-ui/src/test/java/com/pivotal/cloudnativespringui/CloudNativeSpringUiApplicationTests.java
@@ -1,11 +1,8 @@
-package io.pivotal.cloudnativespringui;
+package com.pivotal.cloudnativespringui;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CloudNativeSpringUiApplicationTests {
 
